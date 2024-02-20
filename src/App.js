07f2f586d045e7472fa9847953804d5e702fc29a
Vishesh Chandra <visheshchandra293@gmail.com>
2024-02-20 18:26:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+//import Child2 from "./components/Child2";
+//import Child1 from "./components/Child1";
+import CreateNote from "./components/notes/CreateNote";
+import ListNotes from "./components/notes/ListNotes";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Child1 />
+      <Child2 /> */}
+      <CreateNote />
+      <ListNotes />
     </div>
   );
 }
---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import messageReducer from "./slices/messageSlice";
+import notesSlice from "./slices/notesSlice";
+export default configureStore({
+    reducer: {
+        messageReducer,
+        notesReducer: notesSlice
+    }
+})
